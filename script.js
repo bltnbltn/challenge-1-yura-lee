7f7d9e0a8cf6b43e8fdbc82cbcd20dc203cdcf7c
Yura lee <yuralee@Yuraui-MacBook-Air.local>
2023-01-17 18:29:29 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function ageChecker() {
+  let age = prompt('What is your age?');
+
+  if (age <= 19) {
+    alert('You can not eligible to play gambling');
+  } else {
+    alert('You can play gambling');
+  }
+}
