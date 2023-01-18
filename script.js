2252e74ff4ba2dd467132b60b7b87ca25fec4c4b
Yura Lee <64767219+yuralees@users.noreply.github.com>
2023-01-17 20:38:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function ageChecker() {
   let age = prompt('What is your age?');
 
   if (age <= 19) {
-    alert('You can not eligible to play gambling');
+    alert('You are not eligible to play gambling');
   } else {
     alert('You can play gambling');
   }
